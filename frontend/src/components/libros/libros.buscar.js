c487ef2799fbb6a173379b265d98462f45ab3ef1
Adriana <aguilaradry7@gmail.com>
2021-11-22 17:35:25 -0500

    Se realiza tabla dinamica para mostrar listado de libros

--- frontend/src/components/libros/libros.buscar.js
+++ frontend/src/components/libros/libros.buscar.js
@@ -2,6 +2,43 @@ import React from "react";
 import { request } from "../helper/helper";
 import { Container, Row } from "react-bootstrap";
 import "./libros.css";
+import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory, { PaginationProvider, PaginationListStandalone } from 'react-bootstrap-table2-paginator';
+
+const products = [
+  {
+    titulo: "Programación orientada a objetos en Java",
+    autor: "FRANCISCO BLASCO",
+    palabrasClaves: "polimorfismo, herencia, POO, encapculamiento, ",
+    fechaPublicacion: "15-05-2019",
+    editorial: "Ediciones de la U",
+    resumen: "El objetivo principal de este libro es presentar al lector los fundamentos de la Programación Orientada a Objetos. Focalizamos la atención en conceptos como el encapsulamiento, la herencia, el polimorfismo, composición, delegación, clases envoltorio, reflection, genéricos, lanzamiento, intercepción, tratamiento de excepciones, etc.",
+    unidades: 2,
+  }
+];
+const columns = [{
+  dataField: 'titulo',
+  text: 'Titulo'
+}, {
+  dataField: 'autor',
+  text: 'Autor'
+}, {
+  dataField: 'palabrasClaves',
+  text: 'Palabras Claves'
+}, {
+  dataField: 'fechaPublicacion',
+  text: 'Fecha Publicacion'
+}, {
+  dataField: 'editorial',
+  text: 'Editorial'
+}, {
+  dataField: 'resumen',
+  text: 'Resumen'
+},
+{
+  dataField: 'unidades',
+  text: 'Unidades'
+}];
 
 export default class librosBuscar extends React.Component {
   constructor(props) {
@@ -9,11 +46,38 @@ export default class librosBuscar extends React.Component {
     this.state = {};
   }
   render() {
+    const options = {
+      custom: true,
+      totalSize: products.length
+    };
     return (
       <Container id="libros-buscar-container">
         <Row>
-          <h1>Página destinada para el home.</h1>
-          <h1>En esta se ubicará el catálogo de libros</h1>
+          <h1>Catálogo de libros</h1>
+        </Row>
+        <Row>
+          <PaginationProvider
+            pagination={ paginationFactory(options) }
+          >
+            {
+              ({
+                paginationProps,
+                paginationTableProps
+              }) => (
+                <div>
+                  <BootstrapTable
+                    keyField="id"
+                    data={ products }
+                    columns={ columns }
+                    { ...paginationTableProps }
+                  />
+                  <PaginationListStandalone
+                    { ...paginationProps }
+                  />
+                </div>
+              )
+            }
+          </PaginationProvider>
         </Row>
       </Container>
     );
--- frontend/src/components/router/router.js
+++ frontend/src/components/router/router.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Login from "../login/login";
+import Login from "../login/Login";
 import PrivateRoute from "../auth/privateroute";
 import Catalogo from "../libros/libros.buscar";
 
