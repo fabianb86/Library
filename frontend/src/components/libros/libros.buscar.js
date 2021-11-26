0b33899694e1b736fe8c5f5ce29691c14c5533af
Alejandra Moreno <kalejita.29@gmail.com>
2021-11-25 22:27:54 -0500

    se agrega el componente grid que permite la conexion con mongo

--- frontend/src/components/libros/libros.buscar.js
+++ frontend/src/components/libros/libros.buscar.js
@@ -1,138 +1,8 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
 import "./libros.css";
-import BootstrapTable from "react-bootstrap-table-next";
-import paginationFactory, {
-  PaginationProvider,
-  PaginationListStandalone,
-  SizePerPageDropdownStandalone,
-} from "react-bootstrap-table2-paginator";
-import ToolkitProvider, { Search } from "react-bootstrap-table2-toolkit";
+import DataGrid from "../grid/grid";
 
-const { SearchBar } = Search;
-
-const products = [
-  {
-    titulo: "Programación orientada a objetos en Java",
-    autor: "FRANCISCO BLASCO",
-    palabrasClaves: "polimorfismo, herencia, POO, encapculamiento, ",
-    fechaPublicacion: "15-05-2019",
-    editorial: "Ediciones de la U",
-    resumen:
-      "El objetivo principal de este libro es presentar al lector los fundamentos de la Programación Orientada a Objetos. Focalizamos la atención en conceptos como el encapsulamiento, la herencia, el polimorfismo, composición, delegación, clases envoltorio, reflection, genéricos, lanzamiento, intercepción, tratamiento de excepciones, etc.",
-    unidades: 2,
-  },
-  {
-    titulo: "Programación orientada a objetos en Java",
-    autor: "FRANCISCO BLASCO",
-    palabrasClaves: "polimorfismo, herencia, POO, encapculamiento, ",
-    fechaPublicacion: "15-05-2019",
-    editorial: "Ediciones de la U",
-    resumen:
-      "El objetivo principal de este libro es presentar al lector los fundamentos de la Programación Orientada a Objetos. Focalizamos la atención en conceptos como el encapsulamiento, la herencia, el polimorfismo, composición, delegación, clases envoltorio, reflection, genéricos, lanzamiento, intercepción, tratamiento de excepciones, etc.",
-    unidades: 2,
-  },
-  {
-    titulo: "Programación orientada a objetos en Java",
-    autor: "FRANCISCO BLASCO",
-    palabrasClaves: "polimorfismo, herencia, POO, encapculamiento, ",
-    fechaPublicacion: "15-05-2019",
-    editorial: "Ediciones de la U",
-    resumen:
-      "El objetivo principal de este libro es presentar al lector los fundamentos de la Programación Orientada a Objetos. Focalizamos la atención en conceptos como el encapsulamiento, la herencia, el polimorfismo, composición, delegación, clases envoltorio, reflection, genéricos, lanzamiento, intercepción, tratamiento de excepciones, etc.",
-    unidades: 2,
-  },
-  {
-    titulo: "Programación orientada a objetos en Java",
-    autor: "FRANCISCO BLASCO",
-    palabrasClaves: "polimorfismo, herencia, POO, encapculamiento, ",
-    fechaPublicacion: "15-05-2019",
-    editorial: "Ediciones de la U",
-    resumen:
-      "El objetivo principal de este libro es presentar al lector los fundamentos de la Programación Orientada a Objetos. Focalizamos la atención en conceptos como el encapsulamiento, la herencia, el polimorfismo, composición, delegación, clases envoltorio, reflection, genéricos, lanzamiento, intercepción, tratamiento de excepciones, etc.",
-    unidades: 2,
-  },
-  {
-    titulo: "Programación orientada a objetos en Java",
-    autor: "FRANCISCO BLASCO",
-    palabrasClaves: "polimorfismo, herencia, POO, encapculamiento, ",
-    fechaPublicacion: "15-05-2019",
-    editorial: "Ediciones de la U",
-    resumen:
-      "El objetivo principal de este libro es presentar al lector los fundamentos de la Programación Orientada a Objetos. Focalizamos la atención en conceptos como el encapsulamiento, la herencia, el polimorfismo, composición, delegación, clases envoltorio, reflection, genéricos, lanzamiento, intercepción, tratamiento de excepciones, etc.",
-    unidades: 2,
-  },
-  {
-    titulo: "Programación orientada a objetos en Java",
-    autor: "FRANCISCO BLASCO",
-    palabrasClaves: "polimorfismo, herencia, POO, encapculamiento, ",
-    fechaPublicacion: "15-05-2019",
-    editorial: "Ediciones de la U",
-    resumen:
-      "El objetivo principal de este libro es presentar al lector los fundamentos de la Programación Orientada a Objetos. Focalizamos la atención en conceptos como el encapsulamiento, la herencia, el polimorfismo, composición, delegación, clases envoltorio, reflection, genéricos, lanzamiento, intercepción, tratamiento de excepciones, etc.",
-    unidades: 2,
-  },
-  {
-    titulo: "Programación orientada a objetos en Java",
-    autor: "FRANCISCO BLASCO",
-    palabrasClaves: "polimorfismo, herencia, POO, encapculamiento, ",
-    fechaPublicacion: "15-05-2019",
-    editorial: "Ediciones de la U",
-    resumen:
-      "El objetivo principal de este libro es presentar al lector los fundamentos de la Programación Orientada a Objetos. Focalizamos la atención en conceptos como el encapsulamiento, la herencia, el polimorfismo, composición, delegación, clases envoltorio, reflection, genéricos, lanzamiento, intercepción, tratamiento de excepciones, etc.",
-    unidades: 2,
-  },
-  {
-    titulo: "Programación orientada a objetos en Java",
-    autor: "FRANCISCO BLASCO",
-    palabrasClaves: "polimorfismo, herencia, POO, encapculamiento, ",
-    fechaPublicacion: "15-05-2019",
-    editorial: "Ediciones de la U",
-    resumen:
-      "El objetivo principal de este libro es presentar al lector los fundamentos de la Programación Orientada a Objetos. Focalizamos la atención en conceptos como el encapsulamiento, la herencia, el polimorfismo, composición, delegación, clases envoltorio, reflection, genéricos, lanzamiento, intercepción, tratamiento de excepciones, etc.",
-    unidades: 2,
-  },
-  {
-    titulo: "Programación orientada a objetos en Java",
-    autor: "FRANCISCO BLASCO",
-    palabrasClaves: "polimorfismo, herencia, POO, encapculamiento, ",
-    fechaPublicacion: "15-05-2019",
-    editorial: "Ediciones de la U",
-    resumen:
-      "El objetivo principal de este libro es presentar al lector los fundamentos de la Programación Orientada a Objetos. Focalizamos la atención en conceptos como el encapsulamiento, la herencia, el polimorfismo, composición, delegación, clases envoltorio, reflection, genéricos, lanzamiento, intercepción, tratamiento de excepciones, etc.",
-    unidades: 2,
-  },
-  {
-    titulo: "Programación orientada a objetos en Java",
-    autor: "FRANCISCO BLASCO",
-    palabrasClaves: "polimorfismo, herencia, POO, encapculamiento, ",
-    fechaPublicacion: "15-05-2019",
-    editorial: "Ediciones de la U",
-    resumen:
-      "El objetivo principal de este libro es presentar al lector los fundamentos de la Programación Orientada a Objetos. Focalizamos la atención en conceptos como el encapsulamiento, la herencia, el polimorfismo, composición, delegación, clases envoltorio, reflection, genéricos, lanzamiento, intercepción, tratamiento de excepciones, etc.",
-    unidades: 2,
-  },
-  {
-    titulo: "Programación orientada a objetos en Java",
-    autor: "FRANCISCO BLASCO",
-    palabrasClaves: "polimorfismo, herencia, POO, encapculamiento, ",
-    fechaPublicacion: "15-05-2019",
-    editorial: "Ediciones de la U",
-    resumen:
-      "El objetivo principal de este libro es presentar al lector los fundamentos de la Programación Orientada a Objetos. Focalizamos la atención en conceptos como el encapsulamiento, la herencia, el polimorfismo, composición, delegación, clases envoltorio, reflection, genéricos, lanzamiento, intercepción, tratamiento de excepciones, etc.",
-    unidades: 2,
-  },
-  {
-    titulo: "Programación orientada a objetos en Java",
-    autor: "FRANCISCO BLASCO",
-    palabrasClaves: "polimorfismo, herencia, POO, encapculamiento, ",
-    fechaPublicacion: "15-05-2019",
-    editorial: "Ediciones de la U",
-    resumen:
-      "El objetivo principal de este libro es presentar al lector los fundamentos de la Programación Orientada a Objetos. Focalizamos la atención en conceptos como el encapsulamiento, la herencia, el polimorfismo, composición, delegación, clases envoltorio, reflection, genéricos, lanzamiento, intercepción, tratamiento de excepciones, etc.",
-    unidades: 2,
-  },
-];
 const columns = [
   {
     dataField: "titulo",
@@ -169,50 +39,18 @@ export default class librosBuscar extends React.Component {
     super(props);
     this.state = {};
   }
+
+  componentDidMount() {}
+
   render() {
-    const options = {
-      custom: true,
-      totalSize: products.length,
-    };
     return (
       <Container id="libros-buscar-container">
-        <>
-          <Row>
-            <h1>Catálogo de libros</h1>
-          </Row>
-          <Row>
-            <ToolkitProvider
-              keyField="id"
-              data={products}
-              columns={columns}
-              search
-            >
-              {(props) => (
-                <div>
-                  <hr />
-
-                  <PaginationProvider pagination={paginationFactory(options)}>
-                    {({ paginationProps, paginationTableProps }) => (
-                      <div>
-                        <SizePerPageDropdownStandalone {...paginationProps} />
-
-                        <SearchBar {...props.searchProps} />
-                        <BootstrapTable
-                          keyField="id"
-                          data={products}
-                          columns={columns}
-                          {...paginationTableProps}
-                          {...props.baseProps}
-                        />
-                        <PaginationListStandalone {...paginationProps} />
-                      </div>
-                    )}
-                  </PaginationProvider>
-                </div>
-              )}
-            </ToolkitProvider>
-          </Row>
-        </>
+        <Row>
+          <h1>Catálogo de libros</h1>
+        </Row>
+        <Row>
+          <DataGrid url="/catalogo" columns={columns} />
+        </Row>
       </Container>
     );
   }
